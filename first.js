37cd4a4b098974c88c0f131109a31ec847abe73e
jogeshsil08 <jogeshsil08@gmail.com>
2026-05-17 20:41:40 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,206 @@
+//Arethmatic oparetors
+// let a = 200;
+// let b = 158;
+// console.log('a-b =' , a - b );
+// console.log("a / b", a / b)
+// console.log("a * b", a * b);
+// console.log("a + b", a + b);
+// console.log("a = ", a ,"& b = ", b);
+
+
+//Unari oparetors
+// let a = 200;
+// let b = 158;
+// console.log("a = ", a ,"& b = ", b);
+
+// a --;
+// console.log("a = " , a);
+// console.log("a++",a++);
+// console.log("++a", ++a);
+
+
+//Assignment operators
+// let a = 5;
+// let b = 2;
+// a **= 4; //a = a ** 4 
+// console.log("a = ",a); // 625
+
+
+//Comparison Operators
+// let a = 5;
+// let b = 5;
+// console.log("5 <= 5", a <= b);
+
+//Logical operators
+// let a = 6;
+// let b = 5;
+// console.log("6 || 5 = ",6 || 5);
+
+
+
+
+//Conditional statement
+// let age = 16;
+
+// if (age >= 18) ;{
+//     console.log("you can vote")
+// }
+
+// if (age < 18) ;{
+//     console.log("you cannot vote")
+// }
+
+
+
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color= "black";
+// }
+
+// if (mode === "light") {
+//     color= "white";
+// }
+
+// console.log(color);
+
+
+
+// let age = 16;
+
+// if (age > 18) {
+//     console.log("vote");
+// } else {
+//     console.log("Not vote")
+// }
+
+// //odd or even
+// let num = 1;
+// if (num% 2 === 0) {
+//     console.log(num, "is even");
+// }else {
+//     console.log(num, "is odd");
+// }
+
+
+// else-if Statement
+
+// let mode = "dark";
+// let color;
+
+
+// if(mode === "dark") {
+//     color = "black";
+// } else if(mode === "blue") {
+//     color = "blue";
+// } else if(mode === "pink") {
+//     color = "pink";
+// } else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// if (mode === "dark") {
+//     console.log(mode);
+// }
+
+
+//Conditional Statements
+
+// let age = 25;
+
+//  let result = age >= 18 ? "adults" : "not adults";
+//  console.log(result); console.log("not adult");
+
+// //Print 1 to 5
+// for(let i=1; i<=5; i++) {
+//     console.log("i = ", i);
+// }
+
+
+
+//Calculate sum of 1 to n
+
+// let some = 0;
+// let n = 5;
+// for(let i=1; i<=5; i++) {
+//     sum = sum + 1; // sum = 1
+// }
+
+// console.log("sum =", sum);
+// console.log("loop has ended");
+
+
+
+// let i = 1;
+// while(i<=5) {
+//     console.log("i=", i);
+//     i++;
+// }
+
+// let i =1;
+// do{
+//     console.log("i=", i);
+//     i++
+// } while(i<=5);
+
+
+//for-of loop
+// let str = "ApnaCollege";
+
+// for(let i of str) {
+//     console.log("i=", i);
+// }
+
+
+
+//for in loop 
+
+// let student = {
+//     name : "Rahul kumar",
+//     age: 20,
+//     cgpa: 7.5,
+//     isPass: true
+// };
+
+// for(let key in student) {
+//     console.log("key=",key, "value=", student[key]);
+// }
+
+
+
+//Practice Qs1
+
+// for(let num=0; num<=100; num++) {
+//     console.log("num=", num);
+// }
+
+// for (let num =0; num <= 100; num++) {
+//     if(num%2 !== 0) { //even number
+//         console.log("num =", num);
+//     }
+// }
+
+
+//Practice Qs2
+// let gameNum = 25;
+
+// let UserNum = prompt("Guess the game number : ");
+
+
+// while(UserNum != gameNum) { //game
+
+//   UserNum =  prompt("You entered wrong number . Guess again : ")
+// }
+
+// console.log("congratulations, you entered the right number");
+
+let str = "ApnaCollege";
+
+
+
+
+
